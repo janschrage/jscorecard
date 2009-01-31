359a45dd742c74b11fb357d90692e71ee23f26d2
Jan Schrage <jan@jschrage.de>
2009-01-31 17:44:34 -0200

    Added target reached percentage

--- app/models/kpi.rb
+++ app/models/kpi.rb
@@ -12,4 +12,25 @@ class Kpi < ActiveRecord::Base
     v=self.achievements.find(:first,:order => "report_date DESC")
     return v.kpivalue unless v.nil?
   end
+  
+  def achievement_percentage(report_date)
+    target = target_value_for(report_date)
+    achieved = last_achieved_value
+    perc=(1000*(target-achieved)/target + 0.5).truncate/10 unless target.nil? or achieved.nil?
+    if !perc.nil?
+      perc = 100 - perc if self.bigger_is_better 
+      perc = 100 + perc if !self.bigger_is_better
+    end
+    return perc
+  end
+
+  def achievement_rating(report_date)
+    percentage = achievement_percentage(report_date)
+    return "N/A" if percentage.nil? 
+    threshold_1 = 90
+    threshold_2 = 60
+    return "ok"   if percentage >= threshold_1
+    return "improve" if percentage >= threshold_2
+    return "failing"
+  end
 end
