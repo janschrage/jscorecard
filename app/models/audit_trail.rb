6d0eacc23156462871abc1385152452dd75bf65b
Jan Schrage <jan@jschrage.de>
2009-02-07 11:34:22 -0200

    Added GPL v3

--- app/models/audit_trail.rb
+++ app/models/audit_trail.rb
@@ -1,6 +1,6 @@
 # JScorecard
 #
-# Copyright (C) 2008 Jan Schrage <jan@jschrage.de>
+# Copyright (C) 2009 Jan Schrage <jan@jschrage.de>
 # 
 # This program is free software: you can redistribute it and/or modify it under the terms of the GNU 
 # General Public License as published by the Free Software Foundation, either version 3 of the License,
