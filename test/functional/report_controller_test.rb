66090e4e7f8dbaabde83e271a1ee14ee5ac5edef
Jan Schrage <Jan@jan-schrages-macbook-air.local>
2010-02-27 11:50:20 -0300

    Functional tests: get index for report, dataentry, admin controllers added.

--- test/functional/report_controller_test.rb
+++ test/functional/report_controller_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class ReportControllerTest < ActionController::TestCase
   # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_should_get_index
+    get :index
+    assert_response :success
   end
 end
