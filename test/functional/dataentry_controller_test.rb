e184893d84a609ce30a061ee869cba550837b3ce
Jan Schrage <Jan@jan-schrages-macbook-air.local>
2010-02-27 11:47:02 -0300

    Existing functional tests works now.

--- test/functional/dataentry_controller_test.rb
+++ test/functional/dataentry_controller_test.rb
@@ -2,7 +2,9 @@ require 'test_helper'
 
 class DataentryControllerTest < ActionController::TestCase
   # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_should_get_index
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:dataentry)
   end
 end
